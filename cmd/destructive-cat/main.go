ed6bf17d26017bb090e8bb311f9a048bc5ee136f
Julian Brost <julian@0x4a42.net>
2020-04-24 16:44:11 +0200

    destructive-cat: add confirmation prompt

--- cmd/destructive-cat/main.go
+++ cmd/destructive-cat/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"os"
@@ -12,9 +13,24 @@ func main() {
 	flag.Int64Var(&blockSize, "block-size", DefaultBlockSize, "block size used for hole punching")
 	flag.Parse()
 
-	err := DestructiveCat(flag.Args(), os.Stdout, blockSize)
-	if err != nil {
-		_, _ = fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
+	if flag.NArg() > 0 {
+		files := flag.Args()
+
+		fmt.Fprintf(os.Stderr, "You are about to run destructive-cat on the follwing files:\n")
+		for _, file := range files {
+			fmt.Fprintf(os.Stderr, "  * %q\n", file)
+		}
+		fmt.Fprintf(os.Stderr, "These files will be DELETED. Are you sure? Type YES to continue: ")
+		response, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+		if response != "YES\n" {
+			fmt.Fprintf(os.Stderr, "\nYou did not type YES, bailing out.\n")
+			os.Exit(1)
+		}
+
+		err := DestructiveCat(files, os.Stdout, blockSize)
+		if err != nil {
+			_, _ = fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
 	}
 }
